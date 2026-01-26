e748e53de7d2fe072d2a78b59346f2ef79c423d5
NGUYEN LE <s3777242@rmit.edu.vn>
2026-01-26 13:31:32 +0700

    Initial project setup with DSA solutions and config files
    Add .gitignore and IntelliJ IDEA project configuration files. Implement Java solutions for common DSA problems: Happy Number (Slow and Fast Pointer), Remove Duplicates from Sorted Array, Two Sum, Two Sum II, and Two Sum IV (Two Pointers). Add placeholder for ThreeSum problem.

---
+++ src/TwoPointers/TwoSumII.java
@@ -0,0 +1,24 @@
+package TwoPointers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 167. Two Sum II - Input Array Is Sorted
+ * Given an array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+ * Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+ * You may assume that each input would have exactly one solution and you may not use the same element twice.
+ */
+public class TwoSumII {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
